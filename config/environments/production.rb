5413fd9a21c9480c723e11d5ac2ded09d91ea4c7
priscila cardoso porsch <priscilac.porsch@gmail.com>
2023-08-21 15:18:07 +0200

    we are good

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Rent_a_tool_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://airbnb-mdarbois-2df6fe8522d7.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
