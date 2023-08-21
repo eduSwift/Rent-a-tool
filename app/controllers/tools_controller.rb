28b71d4362f3438f0c4a1bf14d9bc8d2d2f81247
priscila cardoso porsch <priscilac.porsch@gmail.com>
2023-08-21 18:01:13 +0200

    changes

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,23 +1,8 @@
 class ToolsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  def index
-  end
 
   def show
   end
+
+
 end
