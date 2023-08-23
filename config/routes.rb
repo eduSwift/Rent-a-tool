bfc25e59380bf36bb85b64ddf461ac93e5d83537
Marie Darbois <marie.darboiss@gmail.com>
2023-08-23 16:04:36 +0200

    keroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tools#index"
-  get 'search', to: "search#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "pages/home", to: "pages#home"
   resources :tools do
-    get :popup
     resources :bookings, only: %i[new create show destroy]
   end
 end
