156d9f57214cbc01c43770788631e34961963a02
eduardorodriguesdacruz <arpegius2008@yahoo.com>
2023-08-24 12:36:04 +0200

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+daniel = User.create!(
+  first_name: “Daniel”,
+  last_name: Name.last_name,
+  address: Address.city,
+  email: “dani@test.com”,
+  password: 123456
+)
+Tool.create!(
+  name: Faker::Appliance.equipment,
+  description: Faker::Lorem.sentence,
+  price: Faker::Commerce.price(range: 10..100),
+  availability: [true, false].sample,
+  category: [‘beginners’].sample,
+  size: [‘small’,].sample,
+  image: LoremFlickr.image(size: “50x60”, search_terms: [‘tools’]),
+  user: daniel
